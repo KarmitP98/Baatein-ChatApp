ca1f57a4c9fa30436277f09c1a5222aa2c14cc1f
Karmit Personal <karmit199@gmail.com>
2020-10-16 11:27:39 -0400

    Darshboard Complete with Current chat status. Online status per user required.

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -5,97 +5,97 @@ import { DataService } from "../../services/data.service";
 import { Subscription } from "rxjs";
 import { IonContent, ModalController } from "@ionic/angular";
 import { UserInfoComponent } from "../user/user-info/user-info.component";
-import Timestamp = firebase.firestore.Timestamp;
 import { AngularFirestore } from "@angular/fire/firestore";
+import Timestamp = firebase.firestore.Timestamp;
 
 @Component( {
-                selector: "app-chat",
-                templateUrl: "./chat.component.html",
-                styleUrls: [ "./chat.component.scss" ]
+                selector : "app-chat",
+                templateUrl : "./chat.component.html",
+                styleUrls : [ "./chat.component.scss" ]
             } )
 export class ChatComponent implements OnInit, OnDestroy {
-
-    @Input( "data" ) data: { user: UserModel, oUser: UserModel, chat?: ChatModel };
-    @ViewChild( "content" ) ionContent: IonContent;
-    text: string;
-    messages: TextModel[] = [];
-    user: UserModel;
-    oUser: UserModel;
-    chat: ChatModel;
-    chatSub: Subscription;
-
-    constructor( private ds: DataService,
-                 private modalController: ModalController,
-                 private afs: AngularFirestore ) {
-
-
+    
+    @Input( "data" ) data : { user : UserModel, oUser : UserModel, chat? : ChatModel };
+    @ViewChild( "content" ) ionContent : IonContent;
+    text : string;
+    messages : TextModel[] = [];
+    user : UserModel;
+    oUser : UserModel;
+    chat : ChatModel;
+    chatSub : Subscription;
+    
+    constructor( private ds : DataService,
+                 private modalController : ModalController,
+                 private afs : AngularFirestore ) {
+        
+        
     }
-
+    
     ngOnInit() {
         this.user = this.data.user;
         this.oUser = this.data.oUser;
         if ( this.data.chat ) {
             this.chat = this.data.chat;
             this.subToChat( this.data.chat );
-
+            
         } else {
             this.chat = {
-                chatId: this.afs.createId(),
-                texts: [],
-                status: "open",
-                userIds: [ this.user.uId, this.oUser.uId ],
-                users: [ this.user, this.oUser ]
+                chatId : this.afs.createId(),
+                texts : [],
+                status : "open",
+                userIds : [ this.user.uId, this.oUser.uId ],
+                users : [ this.user, this.oUser ]
             };
         }
     }
-
-
-    ngOnDestroy(): void {
+    
+    
+    ngOnDestroy() : void {
         this.chatSub.unsubscribe();
     }
-
-    private subToChat( c: ChatModel ): void {
-        this.chatSub = this.ds.fetchChats( "chatId", "==", c.chatId )
-                           .subscribe( value => {
-                               if ( value?.length ) {
-                                   this.chat = value[0];
-
-                                   setTimeout( () => {
-                                       this.ionContent.scrollToBottom();
-                                   }, 100 );
-                               }
-                           } );
-    }
-
-    sendMessage(): void {
+    
+    sendMessage() : void {
         if ( this.text.length > 0 ) {
-
+            
             if ( this.chat.texts.length === 0 ) {
                 this.ds.addChat( this.chat );
                 this.subToChat( this.chat );
             }
-
-            const tempText: TextModel = {
-                from: this.user.uId,
-                to: this.oUser.uId,
-                data: this.text,
-                status: "sent",
-                lastUpdateTime: Timestamp.now(),
-                sentTime: Timestamp.now(),
-                deleted: false
+            
+            const tempText : TextModel = {
+                from : this.user.uId,
+                to : this.oUser.uId,
+                data : this.text,
+                status : "sent",
+                lastUpdateTime : Timestamp.now(),
+                sentTime : Timestamp.now(),
+                deleted : false
             };
-
+            
             this.chat.texts.push( tempText );
             this.updateChat();
-
+            
             this.text = "";
         }
     }
-
+    
+    async openUserDetails() {
+        const modal = await this.modalController.create( {
+                                                             component : UserInfoComponent,
+                                                             componentProps : { user : this.oUser },
+                                                             swipeToClose : true,
+                                                             animated : true,
+                                                             mode : "md",
+                                                             showBackdrop : true
+                                                         } );
+        
+        return modal.present();
+    }
+    
     updateChat() {
         this.ds.updateChat( this.chat, this.chat.chatId );
     }
-
+    
     updateChatStatus() {
         this.messages.forEach( value => {
             if ( value.status === "sent" && value.from !== this.user.uId ) {
@@ -105,18 +105,18 @@ export class ChatComponent implements OnInit, OnDestroy {
         } );
         this.updateChat();
     }
-
-    async openUserDetails() {
-        const modal = await this.modalController.create( {
-                                                             component: UserInfoComponent,
-                                                             componentProps: { user: this.oUser },
-                                                             swipeToClose: true,
-                                                             animated: true,
-                                                             mode: "md",
-                                                             showBackdrop: true
-                                                         } );
-
-        return modal.present();
+    
+    private subToChat( c : ChatModel ) : void {
+        this.chatSub = this.ds.fetchChats( "chatId", "==", c.chatId )
+                           .subscribe( value => {
+                               if ( value?.length ) {
+                                   this.chat = value[0];
+                
+                                   setTimeout( () => {
+                                       this.ionContent.scrollToBottom();
+                                   }, 100 );
+                               }
+                           } );
     }
-
+    
 }
--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -9,72 +9,76 @@ import { pushTrigger } from "../../shared/animations";
 import { Subscription } from "rxjs";
 import { UsersListComponent } from "../../components/user/users-list/users-list.component";
 
-@UntilDestroy( { checkProperties: true } )
+@UntilDestroy( { checkProperties : true } )
 
 @Component( {
-                selector: "app-dashboard",
-                templateUrl: "./dashboard.page.html",
-                styleUrls: [ "./dashboard.page.scss" ],
-                animations: [ pushTrigger ]
+                selector : "app-dashboard",
+                templateUrl : "./dashboard.page.html",
+                styleUrls : [ "./dashboard.page.scss" ],
+                animations : [ pushTrigger ]
             } )
 export class DashboardPage implements OnInit, OnDestroy {
-
+    
     chat = false;
-    userSub: Subscription;
-    chatSub: Subscription;
-    user: UserModel;
-    users: UserModel[] = [];
-    selectedUser: UserModel;
-    chats: ChatModel[] = [];
-    selectedChat: ChatModel;
-
-    constructor( private route: ActivatedRoute,
-                 public ds: DataService,
-                 private pc: PopoverController,
-                 private mc: ModalController ) {
-
+    userSub : Subscription;
+    chatSub : Subscription;
+    user : UserModel;
+    users : UserModel[] = [];
+    selectedUser : UserModel;
+    chats : ChatModel[] = [];
+    selectedChat : ChatModel;
+    
+    constructor( private route : ActivatedRoute,
+                 public ds : DataService,
+                 private pc : PopoverController,
+                 private mc : ModalController ) {
+        
         const uid = this.route.snapshot.params["uId"];
-
+        
         this.userSub = this.ds.fetchUser()
                            .subscribe( value => {
                                if ( value ) {
                                    this.user = value.filter( usr => usr.uId === uid )[0];
+                                   if ( !this.user.onlineStatus || this.user.onlineStatus === "offline" ) {
+                                       this.user.onlineStatus = "online";
+                                       this.ds.updateUser( this.user );
+                                   }
                                    this.users = value.filter( usr => usr.uId !== uid );
                                }
                            } );
-
+        
         this.chatSub = this.ds.fetchChats( "userIds", "array-contains", uid )
                            .subscribe( value => {
                                if ( value?.length > 0 ) {
                                    this.chats = value;
                                }
                            } );
-
+        
     }
-
+    
     ngOnInit() {
     }
-
-    ngOnDestroy(): void {
+    
+    ngOnDestroy() : void {
         this.userSub.unsubscribe();
     }
-
-    async openMenu( $event: MouseEvent ) {
+    
+    async openMenu( $event : MouseEvent ) {
         this.ngOnDestroy();
         const pop = await this.pc
                               .create( {
-                                           component: MenuComponent,
-                                           event: $event,
-                                           animated: true,
-                                           mode: "md",
-                                           keyboardClose: true,
-                                           backdropDismiss: true,
-                                           componentProps: { uId: this.user.uId }
+                                           component : MenuComponent,
+                                           event : $event,
+                                           animated : true,
+                                           mode : "md",
+                                           keyboardClose : true,
+                                           backdropDismiss : true,
+                                           componentProps : { uId : this.user.uId }
                                        } );
         await pop.present();
     }
-
-    loadChatWith( oUser: UserModel, withChat?: ChatModel ) {
+    
+    loadChatWith( oUser : UserModel, withChat? : ChatModel ) {
         if ( this.selectedUser === oUser ) {
             this.chat = !this.chat;
             this.selectedChat = null;
@@ -82,55 +86,67 @@ export class DashboardPage implements OnInit, OnDestroy {
         } else {
             this.chat = false;
             this.selectedChat = null;
-
+            
             setTimeout( value => {
                 this.selectedUser = oUser;
                 if ( withChat ) {
                     this.selectedChat = withChat;
                 }
                 this.chat = true;
-
+                
             }, 100 );
         }
     }
-
-    loadChat( chat: ChatModel ): void {
+    
+    loadChat( chat : ChatModel ) : void {
         var oUser = this.getOtherUser( chat );
         this.loadChatWith( oUser, chat );
     }
-
-    getOtherUser( chat: ChatModel ): UserModel {
+    
+    getOtherUser( chat : ChatModel ) : UserModel {
         return this.users.filter( usr => usr.uId === chat.userIds.filter( value => value !== this.user.uId )[0] )[0];
     }
-
+    
     async openUsersList() {
-        var temp: UserModel[] = [];
-
+        var temp : UserModel[] = [];
+        
         for ( let i = 0; i < temp.length; i++ ) {
             if ( !this.hasChatsWith( temp[i].uId ) ) {
                 temp.splice( i, 1 );
             }
         }
-
+        
         const modal = await this.mc.create( {
-                                                component: UsersListComponent,
-                                                componentProps: { users: temp },
-                                                mode: "md",
-                                                swipeToClose: true,
-                                                showBackdrop: false,
-                                                animated: true,
-                                                backdropDismiss: false
+                                                component : UsersListComponent,
+                                                componentProps : { users : temp },
+                                                mode : "md",
+                                                swipeToClose : true,
+                                                showBackdrop : false,
+                                                animated : true,
+                                                backdropDismiss : false
                                             } );
-
+        
         await modal.present();
-
+        
         const { data } = await modal.onWillDismiss();
         if ( data?.user ) {
             this.loadChatWith( data.user );
         }
     }
-
-    private hasChatsWith( oUserId: string ): boolean {
+    
+    public getChatUnreadMessages( chat : ChatModel ) : number {
+        var sum = 0;
+        
+        chat.texts.forEach( value => value.status !== "read" ? sum++ : "" );
+        
+        return sum;
+    }
+    
+    public isUserOnline( chat : ChatModel ) : boolean {
+        return this.users.filter( value => value.uId === this.getOtherUser( chat ).uId )[0].onlineStatus === "online";
+    }
+    
+    private hasChatsWith( oUserId : string ) : boolean {
         return this.chats.some( value => value.userIds.some( value1 => value1 === oUserId ) );
     }
 }
