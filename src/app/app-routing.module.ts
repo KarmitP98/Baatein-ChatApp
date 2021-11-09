24df280d04e3aa2211b15c9770efb877cb8a6fc9
KarmitP98 <karmit199@gmail.com>
2021-11-08 19:28:20 -0500

    [Update] Removed all the components and added Assets from the design.
    - Removed all the components to setup a makeover.
    
    Signed-off-by: KarmitP98 <karmit199@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,15 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
-import { LoginGuard } from "./guards/login.guard";
-import { AuthGuard } from "./guards/auth.guard";
 
-const routes: Routes = [
-    {
-        path: "",
-        redirectTo: "login",
-        pathMatch: "full"
-    },
-    {
-        path: "login",
-        loadChildren: () => import("./pages/login/login.module").then( m => m.LoginPageModule ),
-        canActivate: [ LoginGuard ]
-    },
-    {
-        path : "signup",
-        loadChildren : () => import("./pages/signup/signup.module").then( m => m.SignupPageModule ),
-        canActivate : [ LoginGuard ]
-    },
-    {
-        path : ":uId",
-        loadChildren : () => import("./pages/dashboard/dashboard.module").then( m => m.DashboardPageModule ),
-        canActivate : [ AuthGuard ],
-        canDeactivate : [ AuthGuard ]
-    }
+const routes : Routes = [
+    {}
 
 ];
 
 @NgModule( {
-               imports: [
-                   RouterModule.forRoot( routes, { preloadingStrategy: PreloadAllModules } )
+               imports : [
+                   RouterModule.forRoot( routes, { preloadingStrategy : PreloadAllModules } )
                ],
-               exports: [ RouterModule ]
+               exports : [ RouterModule ]
            } )
 export class AppRoutingModule {}
