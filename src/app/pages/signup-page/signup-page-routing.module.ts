4af7264a3f87de1dc86e78e9d05f59eb367ff5a8
KarmitP98 <karmit199@gmail.com>
2021-11-11 19:33:23 -0500

    [Update] Minor update to routing module export.
    Signed-off-by: KarmitP98 <karmit199@gmail.com>

--- src/app/pages/signup-page/signup-page-routing.module.ts
+++ src/app/pages/signup-page/signup-page-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { SignupPageComponent } from "./signup-page.component";
 
 const routes : Routes = [
@@ -8,9 +8,7 @@ const routes : Routes = [
 ];
 
 @NgModule( {
-               imports : [
-                   RouterModule.forRoot( routes, { preloadingStrategy : PreloadAllModules } )
-               ],
+               imports : [ RouterModule.forChild( routes ) ],
                exports : [ RouterModule ]
            } )
 export class SignupPageRoutingModule {}
