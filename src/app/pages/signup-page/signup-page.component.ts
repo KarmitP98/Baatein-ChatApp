89dd55356769d794983309f85cf560ca28b68af7
KarmitP98 <karmit199@gmail.com>
2021-11-16 18:22:34 -0500

    [Feat] Implemented Slide changes for Signup and checking for emails on signup page.
    Signed-off-by: KarmitP98 <karmit199@gmail.com>

--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
 import { AuthService } from "../../services/auth.service";
 import { NgForm } from "@angular/forms";
 import { UserModel } from "../../models/UserModel";
 import { IonSlides } from "@ionic/angular";
+import { UserService } from "../../services/user.service";
 
 @Component( {
                 selector : "app-signup-page",
                 templateUrl : "./signup-page.component.html",
                 styleUrls : [ "./signup-page.component.scss" ]
             } )
-export class SignupPageComponent implements OnInit {
+export class SignupPageComponent implements OnInit, AfterViewInit {
     
     email = "";
     password = "";
@@ -17,13 +18,23 @@ export class SignupPageComponent implements OnInit {
     name = "";
     @ViewChild( "emailForm", { static : false } ) emailForm : NgForm;
     @ViewChild( "userNameForm", { static : false } ) userNameForm : NgForm;
-    @ViewChild( "slides", { static : false } ) slides : IonSlides;
+    @ViewChild( "slides" ) slides : IonSlides;
     processing = false;
+    allowSlideBack : boolean = false;
+    allowSlideForward : boolean = false;
+    slideOptions = { initialSlide : 0 };
     
-    constructor( private authService : AuthService ) { }
+    constructor( private authService : AuthService, private userService : UserService ) { }
     
     async ngOnInit() {
     }
+    
+    async ngAfterViewInit() : Promise<void> {
+        await this.slides.lockSwipes( true );
+        await this.slides.lockSwipeToNext( this.allowSlideForward );
+        await this.slides.lockSwipeToPrev( this.allowSlideBack );
+    }
+    
     
     async handleUserNameSubmit() {
         this.processing = true;
@@ -40,11 +51,25 @@ export class SignupPageComponent implements OnInit {
         this.userNameForm.resetForm();
     }
     
-    handleEmailSubmit() : void {
+    async handleEmailSubmit() : Promise<void> {
         this.processing = true;
         
         // TODO: Implement checking if a registered email exists.
-        this.step = 1;
+        const userExists = await this.userService.checkIfUserEmailExists( this.email );
+    
+        if ( userExists ) {
+            this.step = 0;
+            this.allowSlideForward = false;
+        } else {
+            this.step = 1;
+            this.allowSlideForward = true;
+            await this.slides.slideNext();
+        }
+        await this.slides.lockSwipeToNext( this.allowSlideForward );
         this.processing = false;
+    }
+    
+    showPager() : boolean {
+        return this.allowSlideForward;
     }
 }
