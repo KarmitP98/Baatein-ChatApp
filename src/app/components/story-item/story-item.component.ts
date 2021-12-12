a3a0b1435866fafac096f51fb374e2e86ed04ade
KarmitP98 <karmit199@gmail.com>
2021-12-11 21:30:25 -0500

    Created styling for stories and stuff.
    Signed-off-by: KarmitP98 <karmit199@gmail.com>

--- src/app/components/story-item/story-item.component.ts
+++ src/app/components/story-item/story-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { UserModel } from "../../models/UserModel";
+import { StoryModel } from "../../models/StoryModel";
 
 @Component( {
                 selector : "app-story-item",
@@ -7,14 +9,21 @@ import { Component, Input, OnInit } from "@angular/core";
             } )
 export class StoryItemComponent implements OnInit {
     
-    @Input() story : any;
+    @Input() story : StoryModel;
     @Input() type : "story" | "add" = "story";
     loading = true;
+    storyUser : UserModel;
     
     constructor() { }
     
     ngOnInit() {
         if ( this.story || this.type ) {
+            this.story?.createdBy?.get()?.then( ( value ) => {
+                if ( value.exists() ) {
+                    this.storyUser = value.val();
+                }
+                this.loading = false;
+            } );
             this.loading = false;
         }
     }
@@ -29,6 +38,10 @@ export class StoryItemComponent implements OnInit {
     
     viewStory = () => {
         // View Story logic goes here...
+    };
+    
+    getUserProfilePic = () => {
+        return this.storyUser?.profilePic || "assets/Avatars/user-default.jpg";
     };
     
 }
